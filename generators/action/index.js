66b11cf62775b28fc60895c171339e61509ac5ca
james <zetlen@gmail.com>
2015-10-23 17:20:02 -0500

    Support multiple custom functions per action

--- generators/action/index.js
+++ generators/action/index.js
@@ -16,6 +16,13 @@ function getActionType(actionName) {
   return actionName.split('.')[0];
 }
 
+function createActionName(action) {
+  return {
+    name: action,
+    value: action
+  };
+}
+
 module.exports = yeoman.generators.Base.extend({
 
   constructor: function() {
@@ -145,96 +152,206 @@ module.exports = yeoman.generators.Base.extend({
     }
   },
 
-  prompting: function() {
+  prompting: {
 
-    function createActionName(action) {
-      return {
-        name: action,
-        value: action
-      };
-    }
+    getActions: function() {
 
-    function actionIsInDomain(name, domain) {
-      var withoutType = name.split('.').slice(1).join('.');
-      return withoutType.indexOf(domain) === 0;
-    }
+      function actionIsInDomain(name, domain) {
+        var withoutType = name.split('.').slice(1).join('.');
+        return withoutType.indexOf(domain) === 0;
+      }
 
-    var self = this;
+      var self = this;
 
-    var done = this.async();
+      var done = this.async();
 
-    var prompts;
+      var prompts;
 
-    var actionNameArgs = this.actionNames;
+      var actionNameArgs = this.actionNames;
 
-    if (actionNameArgs) {
+      if (actionNameArgs) {
 
-      this._actionNames = [];
-      this._domains = [];
+        this._actionNames = [];
+        this._domains = [];
 
-      actionNameArgs.forEach(function(name) {
-        var domain = helpers.getDomainFromActionName(self._actionDefs, name);
-        if (!domain) {
-          throw new Error('No domain found for action name ' + name + '. It appears to be an invalid action.');
-        }
-        if (this._domains.indexOf(domain) === -1) {
-          this._domains.push(domain);
-        }
-        this._actionNames.push(createActionName(name, domain));
-      });
-
-      done();
-
-    } else {
-
-      prompts = [{
-        type: 'checkbox',
-        name: 'domains',
-        message: 'Choose domains:',
-        choices: self._actionDefs.domains,
-        validate: function(chosen) {
-          return chosen.length > 0 || 'Please choose at least one domain to scaffold.';
-        },
-        default: this.options.domains || this.config.get('domains')
-      }].concat(self._actionDefs.domains.map(function(domain) {
-        return {
-          type: 'checkbox',
-          name: domain,
-          message: 'Actions for domain ' + chalk.bold(domain),
-          choices: self._availableActions.filter(function(action) {
-            return actionIsInDomain(action.action, domain);
-          }).map(function(action) {
-            return createActionName(action.action, domain);
-          }),
-          default: (self.options.actionNames || self.implementedActions).filter(function(name) {
-            return actionIsInDomain(name, domain);
-          }),
-          when: function(props) {
-            return props.domains.indexOf(domain) !== -1;
+        actionNameArgs.forEach(function(name) {
+          var domain = helpers.getDomainFromActionName(self._actionDefs, name);
+          if (!domain) {
+            throw new Error('No domain found for action name ' + name + '. It appears to be an invalid action.');
           }
-        };
-      }));
-      
-      if (!this.options['skip-prompts']) {
-        this.prompt(prompts, function(answers) {
-          self._domains = answers.domains;
-          delete answers.domains;
-          self._actionNames = Object.keys(answers).reduce(function(m, k) {
-            return m.concat(answers[k]);
-          }, []);
+          if (this._domains.indexOf(domain) === -1) {
+            this._domains.push(domain);
+          }
+          this._actionNames.push(createActionName(name, domain));
+        });
+
+        done();
+
+      } else {
+
+        prompts = [{
+          type: 'checkbox',
+          name: 'domains',
+          message: 'Choose domains:',
+          choices: self._actionDefs.domains,
+          validate: function(chosen) {
+            return chosen.length > 0 || 'Please choose at least one domain to scaffold.';
+          },
+        default: this.options.domains || this.config.get('domains')
+        }].concat(self._actionDefs.domains.map(function(domain) {
+          return {
+            type: 'checkbox',
+            name: domain,
+            message: 'Actions for domain ' + chalk.bold(domain),
+            choices: self._availableActions.filter(function(action) {
+              return actionIsInDomain(action.action, domain);
+            }).map(function(action) {
+              return createActionName(action.action, domain);
+            }),
+            default: (self.options.actionNames || self.implementedActions).filter(function(name) {
+              return actionIsInDomain(name, domain);
+            }),
+            when: function(props) {
+              return props.domains.indexOf(domain) !== -1;
+            }
+          };
+        }));
+
+        // multiple custom function support
+        prompts.push({
+          type: 'list',
+          name: 'createMultiple',
+          message: 'Create multiple custom functions per action, or one ' +
+                   'custom function per action? (You can manually change ' +
+                   'this later.)',
+          choices: [
+            {
+              name: 'Multiple (advanced mode)',
+              value: true,
+            },
+            {
+              name: 'One (simple mode)',
+              value: false
+            }
+          ],
+          when: function(answers) {
+            return answers.domains.some(function(domain) {
+              return answers[domain].some(function(action) {
+                return self._actionsMap[action]
+                        .supportsMultipleCustomFunctions;
+              })
+            });
+          }
+        });
+
+        if (!this.options['skip-prompts']) {
+          this.prompt(prompts, function(answers) {
+            self._domains = answers.domains;
+            delete answers.domains;
+            self._createMultiple = answers.createMultiple;
+            delete answers.createMultiple;
+            self._actionNames = Object.keys(answers).reduce(function(m, k) {
+              return m.concat(answers[k]);
+            }, []);
+            done();
+          });
+        }
+
+      }
+
+    }, 
+    createMultipleActionsFor: function() {
+      var self = this;
+      var done;
+      if (self._createMultiple) {
+        done = self.async();
+        self.prompt([
+          {
+            type: 'checkbox',
+            name: 'createMultipleFor',
+            message: 'Create multiple custom functions for which actions?',
+            choices: self._actionNames.filter(function(action) {
+              return self._actionsMap[action].supportsMultipleCustomFunctions;
+            }).map(createActionName)
+          }
+        ], function(answers) {
+          self._createMultipleFor = answers.createMultipleFor;
           done();
         });
       }
-
+    },
+    getMultipleActionNames: function() {
+      var self = this;
+      var done;
+      var seenNames = {};
+      if (self._createMultipleFor && self._createMultipleFor.length > 0) {
+        done = self.async();
+        helpers.remark(this, 
+                       'For each of the following actions, please provide ' +
+                       'a list of custom function names.\nThey must be ' + 
+                       'unique, comma separated, and contain no spaces.');
+        helpers.remark(this,
+                       'For instance, to create three custom functions ' +
+                       'named "alpha", "beta", and "gamma", type ' +
+                        chalk.yellow('alpha,beta,gamma') + '.');
+        self.prompt(
+          self._createMultipleFor.map(function(name) {
+            return {
+              type: 'input',
+              name: name,
+              message: 'Custom function names for ' + chalk.bold.yellow(name) +
+                       ':',
+              validate: function(value) { 
+                if (!value || value.length == 0) {
+                  return 'Please enter at least one name for a custom ' +
+                         'function.';
+                }
+                var seen = value.split(',').filter(function(v) {
+                  return !!seenNames[v];
+                });
+                switch (seen.length) {
+                  case 0:
+                    return true;
+                  case 1:
+                    return 'You have already used the custom function name ' +
+                           chalk.yellow(seen[0]) + '. Please select a ' +
+                           'different name.';
+                  default:
+                    return 'You have already used the custom function ' +
+                           'names ' + chalk.yellow(seen.join(', ')) + '. ' +
+                           ' Please select different names.'
+                }
+              },
+              filter: function(value) { 
+                // big cheat but helps with validation;
+                // a side effect in the filter function, where we keep track
+                // of custom function names we have already seen.
+                var values = value.split(',').map(function(v) {
+                  v = v.trim();
+                  seenNames[v] = true;
+                  return v;
+                });
+                return values;
+              }
+            }
+          }),
+          function(answers) {
+            self._multipleCustomFunctions = answers;
+            done();
+          }
+        )
+      } else {
+        self._multipleCustomFunctions = {};
+      }
     }
-
   },
 
   configuring: function() {
     this._actions = this._actionNames.map(function(name) {
       return {
         name: name,
-        domain: this._actionsMap[name].domain
+        domain: this._actionsMap[name].domain,
+        customFunctionNames: this._multipleCustomFunctions[name] || [name]
       };
     }.bind(this));
     this.config.set('domains', this._domains);
@@ -258,17 +375,21 @@ module.exports = yeoman.generators.Base.extend({
           actions: thisDomainsActions
         });
       thisDomainsActions.forEach(function(action) {
-        var implPath = self.destinationPath('assets/src/domains/' + domain + '/' + action.name + '.js');
-        if ((self.options.overwriteAll || !self.fs.exists(implPath)) && (doNotWrite.indexOf(action.name) === -1)) {
-          self.fs.copyTpl(
-            self.templatePath('_action_implementation.jst'),
-            implPath, {
-              action: action,
-              actionType: getActionType(action.name),
-              context: JSON.stringify(self._actionsMap[action.name].context, null, 2)
-            }
-          );
-        }
+        action.customFunctionNames.forEach(function(customFunctionName) {
+          var implPath = self.destinationPath('assets/src/domains/' + 
+                         domain + '/' + customFunctionName + '.js');
+          if ((self.options.overwriteAll || !self.fs.exists(implPath)) &&
+              (doNotWrite.indexOf(action.name) === -1)) {
+            self.fs.copyTpl(
+              self.templatePath('_action_implementation.jst'),
+              implPath, {
+                action: action,
+                actionType: getActionType(action.name),
+                context: JSON.stringify(self._actionsMap[action.name].context, null, 2)
+              }
+            );
+          }
+        })
       });
     });
 
@@ -282,32 +403,37 @@ module.exports = yeoman.generators.Base.extend({
 
       helpers.remark(this, 'Installing ' + this.options.testFramework);
       this._actions.forEach(function(action) {
+        action.customFunctionNames.forEach(function(customFunctionName) {
 
-        var testPath = this.destinationPath('assets/test/' + action.name + '.t.js');
-        var actionType = getActionType(action.name);
-        var actionConfig = this._actionsMap[action.name];
+          var testPath = this.destinationPath(
+            'assets/test/' + customFunctionName + '.t.js');
+          var actionType = getActionType(action.name);
+          var actionConfig = this._actionsMap[action.name];
 
-        if ((doNotWrite.indexOf(action.name) === -1) && (this.options.overwriteAll || !this.fs.exists(testPath))) {
+          if ((doNotWrite.indexOf(action.name) === -1) &&
+              (this.options.overwriteAll || !this.fs.exists(testPath))) {
 
-          try {
-            this.fs.copyTpl(
-              this.templatePath('test/' + this.options.testFramework + '_' + actionType + '.jst'),
-              testPath,
-              {
-                action: action, 
-                context: actionConfig.context
+            try {
+              this.fs.copyTpl(
+                this.templatePath('test/' + this.options.testFramework + '_' + actionType + '.jst'),
+                testPath,
+                {
+                  action: action, 
+                  context: actionConfig.context
+                }
+              );
+            } catch(e) {
+              helpers.lament(this, 'There was an error creating unit tests. This may mean that there is missing metadata for one or more of the actions you chose.');
+              if (this.options.internal) {
+                helpers.lament(this, 'Examine the mozuxd-metadata package to make sure there is complete metadata for the context object for this action: ' + action.name + '. The original error is: ');
+              } else {
+                helpers.lament(this, 'Please contact Mozu Support to report this issue writing tests for action ' + action.name + ':');
               }
-            );
-          } catch(e) {
-            helpers.lament(this, 'There was an error creating unit tests. This may mean that there is missing metadata for one or more of the actions you chose.');
-            if (this.options.internal) {
-              helpers.lament(this, 'Examine the mozuxd-metadata package to make sure there is complete metadata for the context object for this action: ' + action.name + '. The original error is: ');
-            } else {
-              helpers.lament(this, 'Please contact Mozu Support to report this issue writing tests for action ' + action.name + ':');
+              throw e;
             }
-            throw e;
           }
-        }
+
+        }.bind(this));
 
       }.bind(this));
     }
